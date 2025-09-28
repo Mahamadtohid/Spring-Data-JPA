d53e6c5b546426bfed594868152fd00bef499446
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-09-28 15:37:28 +0530

    Setup and Configuration

---
+++ src/main/java/com/example/RydeReviewService/RydeReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RydeReviewService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RydeReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RydeReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RydeReviewService/RydeReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RydeReviewService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RydeReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
