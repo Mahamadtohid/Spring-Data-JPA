52bdd0b522a2961c568a3a0244e708ce5d589a6a
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-09-28 19:00:03 +0530

    Added repository package

--- src/main/java/com/example/RydeReviewService/models/Review.java
+++ src/main/java/com/example/RydeReviewService/models/Review.java
@@ -1,12 +1,38 @@
 package com.example.RydeReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.NonNull;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder //Apples the builder pattern to create an object of An class Review
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name="bookingreview")
 public class Review {
 
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    private double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
 }
---
+++ src/main/java/com/example/RydeReviewService/repositories/ReviewRepository.java
@@ -0,0 +1,4 @@
+package com.example.RydeReviewService.repositories;
+
+public class ReviewRepository {
+}
