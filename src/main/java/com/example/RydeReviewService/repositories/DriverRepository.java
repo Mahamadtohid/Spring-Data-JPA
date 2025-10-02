b47ce76dc71a8f0c5fd1eddacdaa186b94f50285
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-10-03 00:33:41 +0530

    flyway Setup for DB Migration

--- src/main/java/com/example/RydeReviewService/repositories/DriverRepository.java
+++ src/main/java/com/example/RydeReviewService/repositories/DriverRepository.java
@@ -1,6 +1,5 @@
 package com.example.RydeReviewService.repositories;
 
-import com.example.RydeReviewService.models.Booking;
 import com.example.RydeReviewService.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
